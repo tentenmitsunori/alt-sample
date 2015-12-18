69551cdb42573846970bd23e2630a6d8ce3b9122
MitsunoriNanno <tentenmitsunori@gmail.com>
2015-12-18 19:26:32 +0900

    delete accessToken url

--- app/assets/javascripts/actions/NameActions.js.jsx
+++ app/assets/javascripts/actions/NameActions.js.jsx
@@ -7,7 +7,7 @@
 			fetchNames() {
 				var that = this
 				$.ajax({
-						url: "https://staging-app-dietcoach.finc.co.jp/v3/dashboard/weight_graph?access_token=RFW8sksqxSsu1zYsZd8-&start_weight=50&end_weight=60&start_date=2015-12-12&end_date=2016-2-3&period_days=30",
+						url: "",
 					dataType: 'json',
 					success:function(res){
 						console.log(res);
